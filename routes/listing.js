078f22d5b768d817bf046fe8d6c9ec64f1e37588
YawarAbdullah <yawarmallick890@gmail.com>
2025-07-15 05:24:56 +0530

    Revert: removed Airbnb type/category feature

--- routes/listing.js
+++ routes/listing.js
@@ -3,39 +3,33 @@ const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
-const multer  = require('multer');
-const {storage} = require("../cloudConfig.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
 const upload = multer({ storage });
-
 
 router
   .route("/")
   .get(wrapAsync(listingController.index))
   .post(
     isLoggedIn,
-    upload.single('listing[image]'),
+    upload.single("listing[image]"),
     validateListing,
     wrapAsync(listingController.createListing)
   );
 
-//New Route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 router.route("/:id")
   .get(wrapAsync(listingController.showListing))
-  .put(isLoggedIn, isOwner,upload.single('listing[image]'), validateListing, wrapAsync(listingController.updateListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
   .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
-
-//Edit Route
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
-// Show listings by category
-router.get("/category/:type", async (req, res) => {
-  const { type } = req.params;
-  const listings = await Listing.find({ category: type });
-
-  res.render("listings/index", { listings, type });
-});
-
 
 module.exports = router;
